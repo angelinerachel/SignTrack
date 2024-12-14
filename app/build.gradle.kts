f11c72b09d8f714bc91ad5b7b7d9dd76624c7dbc
angelinerachel <angelinerachelll@gmail.com>
2024-12-14 09:11:22 +0700

    Add CameraX dependencies and fix layout issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +39,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //Core CameraX Library
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+
+    // CameraX PreviewView
+    implementation("androidx.camera:camera-view:1.3.1")
 }
